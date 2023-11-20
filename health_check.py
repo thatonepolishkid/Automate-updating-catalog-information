b5efa1ce264f7555ab9b92ca43124b2c2a1a2f86
Marcin Malek <martinmalek1@gmail.com>
2023-11-19 19:56:53 -0500

    Added virtual memory check - refactored messages Changed messages to correspond to the format which the lab wants them to be. Added the Virtual memory check through the psutil module, for if the memory is less than 500MB, it will return false giving the error.

--- health_check.py
+++ health_check.py
@@ -24,11 +24,13 @@ def check_root_full():
 def check_no_network():
     """Returns True if it fails to resolve Google's URL, False otherwise"""
     try:
-        socket.gethostbyname("www.google.com")
+        socket.gethostbyname("localhost")
         return False
     except Exception:
         return True
 
+def check_virtual_memory():
+    return psutil.virtual_memory() >= 500
 
 def check_cpu_usage():
     """Checks cpu usage over 2 seconds, and gets the average. If cpu usage under 80% returns True, else returns False"""
@@ -39,9 +41,10 @@ def check_cpu_usage():
 
 def main():
     checks = [
-        (check_root_full, "Root partition Full"),
-        (check_no_network, "No working network."),
-        (check_cpu_usage, "Caution CPU usage above 80%!"),
+        (check_root_full, "Error - Available disk space is less than 20%"),
+        (check_no_network, "Error - localhost  cannot be resolved to [127.0.0.1]"),
+        (check_cpu_usage, "Error - cpu usage is over 80%"),
+        (check_virtual_memory, "Error - Available memmory is less than 500MB")
     ]
     everything_ok = True
     for check, msg in checks:
