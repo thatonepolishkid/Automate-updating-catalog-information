c5d5645e4c8ca8373bab857a11fcc7b65453810d
Marcin Malek <martinmalek1@gmail.com>
2023-11-20 09:54:45 -0500

    Adding the table to be created.

--- create_pdf.py
+++ create_pdf.py
@@ -1,4 +1,30 @@
+#!/usr/bin/env python3
+
 import reports
 import emails
 import os
 import datetime
+import reportlab
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+
+def generate(filename, title, additional_info, table_data):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
+  table_style = [('GRID', (0,0), (-1,-1), 1, colors.black),
+                ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+                ('ALIGN', (0,0), (-1,-1), 'CENTER')]
+  report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+  empty_line = Spacer(1,20)
+  report.build([report_title, empty_line, report_info, empty_line, report_table])
+
+def fruit_dict_to_table(fruit_data):
+  """Turns the data in car_data into a list of lists."""
+  table_data = [["name",  "weight"]]
+  table_data.extend([item['name'], item['weight']] for item in fruit_data['fruit'])
+  return table_data
