55f91ebc10c77300c932e670c94f0aa3c684d3bd
Marcin Malek <martinmalek1@gmail.com>
2023-11-20 09:57:50 -0500

    Plugging in the endgoal in the main function

--- create_pdf.py
+++ create_pdf.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
+import sys
+from reports import generate as report
+from emails import generate as email_generate
+from emails import send as email_send
 
-import reports
-import emails
-import os
-import datetime
-import reportlab
 
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
 
-def generate(filename, title, additional_info, table_data):
-  styles = getSampleStyleSheet()
-  report = SimpleDocTemplate(filename)
-  report_title = Paragraph(title, styles["h1"])
-  report_info = Paragraph(additional_info, styles["BodyText"])
-  table_style = [('GRID', (0,0), (-1,-1), 1, colors.black),
-                ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
-                ('ALIGN', (0,0), (-1,-1), 'CENTER')]
-  report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
-  empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line, report_table])
+
 
 def fruit_dict_to_table(fruit_data):
   """Turns the data in car_data into a list of lists."""
   table_data = [["name",  "weight"]]
   table_data.extend([item['name'], item['weight']] for item in fruit_data['fruit'])
   return table_data
+
+def main(argv):
+    data = load_data('file location')
+    summary = process_data(data)
+    new_summary = ''.join(summary)
+    print(summary)
+    report("/tmp/cars.pdf", "Cars Report", new_summary, fruit_dict_to_table(data))
+    message = email_generate("automation@example.com", "username@example.com", "sales summary for last month", new_summary, "/tmp/cars.pdf")
+    email_send(message)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
